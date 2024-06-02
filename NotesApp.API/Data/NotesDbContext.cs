70ff65783997fdbdec0f4925c97a4f887302e31c
Your Name <you@example.com>
2024-06-02 12:54:29 +0530

    dependency injection

---
+++ NotesApp.API/Data/NotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.API.Models.DomainModels;
+
+namespace NotesApp.API.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
